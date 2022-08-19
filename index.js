ca205bb8612a2fd7b0a0b4c74c406ea44fa0809e
Ajeet <ajeetsingh8139@gmail.com>
2022-08-19 16:52:42 +0530

    save the changes

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const port = 8000;
 
 // use exprss router
 app.use('/', require('./routes'));
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running the server: ${err}`);
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,9 @@ const homeController = require('../controllers/home_controller');
 console.log('router loaded');
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+// for any further router, access from here
+// router.use('/routerName', require('./routerfile'));
 
 module.exports = router;
