f56e7dd8b4ad09048373b198e07fd3ac56394038
Ajeet <ajeetsingh8139@gmail.com>
2022-08-21 17:14:50 +0530

    Setting up Mongo store for session

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
-const app = express();
+const MongoStore = require('connect-mongo')(session);
 
 
-
-app.use(express.json());
 app.use(express.urlencoded());
 
 
@@ -29,6 +28,8 @@ app.set('layout extractScripts', true);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+
+// mongo store to used to the session cooke in the db
 app.use(session({
     name: 'codeial',
     // TODO change the secret before deployment in production mode
@@ -37,7 +38,16 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
-    }
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        },
+        function(err){
+            console.log(err || 'connect-mongodb setup ok');
+        }
+    )
 }));
 
 app.use(passport.initialize());
