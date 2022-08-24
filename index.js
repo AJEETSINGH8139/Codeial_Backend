cea90cf648be291345b93102d00dedd4a910dd7a
Ajeet <ajeetsingh8139@gmail.com>
2022-08-24 08:19:17 +0530

    Commit for scss file

--- index.js
+++ index.js
@@ -8,8 +8,18 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
+const sassMiddleware = require('node-sass-middleware');
 
 
+app.use(sassMiddleware({
+    src: '/assets/scss',
+    dist: '/assets/css',
+    debug: true,
+    outputStyle: 'expanded',
+    prefix: '/css'
+}));
+
+//app.use(bodyParser.json());
 app.use(express.urlencoded());
 
 
