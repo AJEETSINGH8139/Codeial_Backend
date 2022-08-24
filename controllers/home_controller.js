641302a00696ef3df3679e4adb631e76530a3abe
Ajeet <ajeetsingh8139@gmail.com>
2022-08-24 22:04:11 +0530

    Commit for post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,26 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home',{
-        title: "Home"
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codial | Home",
+    //         posts: posts
+    //     });
+    // });
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "Codial | Home",
+            posts: posts
+        });
     });
+    // return res.render('home',{
+    //     title: "Home"
+    // });
 }
 
 // module.exports.actionName = function(req, res){}
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ console.log('router loaded');
 
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
 
 // for any further router, access from here
 // router.use('/routerName', require('./routerfile'));
